52db7d2477827b1bd9197bc1ccb215df2ab11acc
aleksandresirbiladze <pavle82sirbiladze@gmail.com>
2024-12-13 21:48:03 +0400

    second commit

---
+++ level 09/classwork/cw.py
@@ -0,0 +1,7 @@
+#შემოატანინეთ მომხმარებელს ორი input, რომელსაც float() ფუნქციის გამოყენებით გადააქცევთ ათწილადად, საბოლოოდ მოახდინეთ მათემატიკური ოპერაციები ამ ორ რიცხვს შორის
+number = int(input("text a number: "))
+number2 = int(input("text a number: "))
+print(number + number2)
+print(number - number2)
+print(number * number2)
+print(number / number2)
---
+++ level 09/homework/hw1.py
@@ -0,0 +1,3 @@
+#შემოატანინეთ მომხმარებელს username და დაპრინტეთ: "Hello, (მისი username) !"
+username = input("text your surname: ")
+print("hello" +" "+ username)
