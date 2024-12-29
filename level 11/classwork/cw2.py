77ac37ec9e041faf03a74a33835842d5976ffc7d
aleksandresirbiladze <pavle82sirbiladze@gmail.com>
2024-12-29 19:13:21 +0400

    second commit

---
+++ level 11/classwork/cw2.py
@@ -0,0 +1,8 @@
+#შექმენით 2 ცვლადი ერთნაირი მნიშვნელობით ( სტრინგი ) . შეადარეთ ერთმანეთს
+aleksandre = "name"
+sirbiladze = "surname"
+print(aleksandre>sirbiladze)
+print(aleksandre<sirbiladze)
+print(aleksandre==sirbiladze)
+print(aleksandre>=sirbiladze)
+print(aleksandre<=sirbiladze)
